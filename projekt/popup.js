779fce7460ccb910c5606c1b11a50d7557e6cb4f
neratmatevz <matevz.nerat@gmail.com>
2023-04-10 20:26:05 +0200

    cookie manager, css changes

--- projekt/popup.js
+++ projekt/popup.js
@@ -45,3 +45,90 @@ let fullWords = ["personal data","personal information","name","surname",
     "transmission","processing","sending","cookies","storage","stored",
     "privacy","IP","operating system","device","contact","address"
   ];
+
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  var url = tabs[0].url;
+
+  // Get all cookies for the current URL
+  chrome.cookies.getAll({ url: url }, function(cookies) {
+    var cookieList = document.getElementById("cookie-list");
+
+    // Clear the cookie list before adding the updated cookies
+    cookieList.innerHTML = "";
+
+    // Add a checkbox for each cookie
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+
+      var li = document.createElement("li");
+      var checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.value = cookie.name;
+      li.appendChild(checkbox);
+      li.appendChild(document.createTextNode(cookie.name + " (" + cookie.domain + ")"));
+      cookieList.appendChild(li);
+    }
+
+    preveriDomenoCookie(cookies);
+
+    // Add a click event listener to the delete button
+    var deleteButton = document.getElementById("delete-cookies-button");
+    deleteButton.addEventListener("click", function() {
+      var selectedCookies = document.querySelectorAll("input[type=checkbox]:checked");
+
+      // Remove each selected cookie
+      for (var i = 0; i < selectedCookies.length; i++) {
+        var cookieName = selectedCookies[i].value;
+
+        chrome.cookies.remove({ url: url, name: cookieName }, function() {
+          console.log("Cookie removed: " + cookieName);
+
+          // Update the cookie list after deleting a cookie
+          chrome.cookies.getAll({ url: url }, function(updatedCookies) {
+            cookieList.innerHTML = "";
+            for (var j = 0; j < updatedCookies.length; j++) {
+              var updatedCookie = updatedCookies[j];
+              var li = document.createElement("li");
+              var checkbox = document.createElement("input");
+              checkbox.type = "checkbox";
+              checkbox.value = updatedCookie.name;
+              li.appendChild(checkbox);
+              li.appendChild(document.createTextNode(updatedCookie.name + " (" + updatedCookie.domain + ")"));
+              cookieList.appendChild(li);
+            }
+            preveriDomenoCookie(cookies);
+          });
+        });
+      }
+    });
+  });
+});
+
+
+function preveriDomenoCookie(cookies){
+  var thirdpartyH3 = document.getElementById("3rdParty");
+  var thirdPartyDomains = [];
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i];
+    if (cookie.domain != window.location.hostname && !thirdPartyDomains.includes(cookie.domain)) {
+      thirdPartyDomains.push(cookie.domain);
+    }
+  }
+  if (thirdPartyDomains.length > 0) {
+    thirdpartyH3.innerHTML = "Stran uporablja piškotke teh domen : ("+ thirdPartyDomains.join(", ")+")!";
+    thirdpartyH3.style.color = "green";
+  } 
+  
+}
+
+//Skrij/Odkrij za cookieje
+const toggleCookiesButton = document.getElementById('toggle-cookies-button');
+const cookieList = document.getElementById('skrij');
+
+toggleCookiesButton.addEventListener('click', function() {
+  if (cookieList.style.display === 'none') {
+    cookieList.style.display = 'block';
+  } else {
+    cookieList.style.display = 'none';
+  }
+});
